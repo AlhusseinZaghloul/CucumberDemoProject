88310835dbab24e4916db3032f8ebc21c7408dd9
AlhusseinZaghloul <husseinzaghloul.77@gmail.com>
2024-01-28 20:24:11 +0200

    adding report after rerun

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/Resources/Features",
+     glue = {"stepDefinition"},
+        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
+        monochrome = true,
+        publish = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
